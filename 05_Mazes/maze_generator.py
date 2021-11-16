38d7300e4122f0cc2b4983652650f8fda09ab187
JohannesHool <hooljohannes@gmail.com>
2021-11-16 10:10:46 +0100

    Move code into maze_generator file

--- 05_Mazes/maze_generator.py
+++ 05_Mazes/maze_generator.py
@@ -6,7 +6,80 @@ from PIL import Image
 import numpy as np
 from random import choice
 
-def generate_maze(width, height):
+def print_pretty(listx):
+    """returns a grid of a list of lists of numbers
+
+    list of list -> grid"""
+    for lists in listx:
+        for i in lists:
+            print(i,end='\t')
+        print()
+    print('*************************************************************************************')
+    
+def generate_example_maze(example = 1):
+    
+    if example == 2:
+        maze_matrix = [
+            [1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        ]
+    if example == 3:
+        maze_matrix = [
+            [1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        ]
+    if example == 1:
+        maze_matrix = [
+            [1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+            [1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+            [1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        ]
+        
+    maze_height = len(maze_matrix)
+    maze_width = len(maze_matrix[0])
+
+    grid = [[0 for x in range(maze_width)] for x in range(maze_height)]    
+
+    im = Image.new("RGB",(len(maze_matrix[0]), len(maze_matrix)))
+    pixels = []
+
+    for x in range(maze_height):
+        for y in range(maze_width):
+            pixel = (0, 0, 0)
+            if maze_matrix[x][y] == 0:
+                pixel = (255, 255, 255)
+
+            pixels.append(pixel)
+
+    im.putdata(pixels)
+    image = im.resize((400,400),Image.NEAREST)
+    return maze_matrix, image, grid
+
+def generate_random_maze(width):
+    height = int(width)
     maze = Image.new('RGB', (2*width + 1, 2*height + 1), 'black')
     pixels = maze.load()
 
@@ -46,8 +119,109 @@ def generate_maze(width, height):
 
             cells[neighbour] = 1
             stack.put(neighbour)
+            
+    pix = np.array(maze)
+    maze_matrix = []
 
-    return maze
+    for row in pix:
+        maz_row = []
+        for pixel in row:
+            if pixel[0] == 255:
+                maz_row.append(0)
+            else:
+                maz_row.append(1)
+        maze_matrix.append(maz_row)
+        
+    image = maze.resize((400,400),Image.NEAREST)
+    
+    maze_height = len(maze_matrix)
+    maze_width = len(maze_matrix[0])
+
+    grid = [[0 for x in range(maze_width)] for x in range(maze_height)]
+
+    return maze_matrix, image, grid
+
+def bfs_solve_maze(maze_matrix, steps=False):
+    maze_height = len(maze_matrix)
+    maze_width = len(maze_matrix[0])
+    solved_maze = [[0 for x in range(maze_width)] for x in range(maze_height)]
+
+    start_x = 0
+    start_y = 1
+
+    end_x = len(maze_matrix)-1
+    end_y = len(maze_matrix[len(maze_matrix) - 1]) - 2
+
+    solved_maze[start_x][start_y] = 1
+
+    step = 0
+
+    while solved_maze[end_x][end_y] == 0:
+        step = step + 1
+        for x in range(maze_height):
+            for y in range(maze_width):
+                if solved_maze[x][y] == step:
+                    if (x - 1) >= 0 and maze_matrix[x-1][y] == 0 and solved_maze[x-1][y] == 0:
+                        solved_maze[x-1][y] = step + 1
+                    if (x + 1) < maze_height and maze_matrix[x + 1][y] == 0 and solved_maze[x + 1][y] == 0:
+                        solved_maze[x+1][y] = step + 1
+                    if (y - 1) >= 0 and maze_matrix[x][y-1] == 0 and solved_maze[x][y-1] == 0:
+                        solved_maze[x][y-1] = step + 1
+                    if (y + 1) < maze_width and maze_matrix[x][y + 1] == 0 and solved_maze[x][y + 1] == 0:
+                        solved_maze[x][y+1] = step + 1
+        if steps:
+            print_pretty(solved_maze)
+            
+    path = []
+    x = end_x
+    y = end_y
+
+    step = solved_maze[x][y]
+    path.append((x, y))
+
+    while step > 1:
+        if x - 1 >= 0 and solved_maze[x-1][y] == step - 1:
+            x -= 1
+            path.append((x, y))
+            step -= 1
+        if x + 1 < maze_height and solved_maze[x+1][y] == step - 1:
+            x += 1
+            path.append((x, y))
+            step -= 1
+        if y - 1 >= 0 and solved_maze[x][y-1] == step - 1:
+            y -= 1
+            path.append((x, y))
+            step -= 1
+        if  y + 1 < maze_width and solved_maze[x][y+1] == step - 1:
+            y += 1
+            path.append((x, y))
+            step -= 1
+            
+    return solved_maze, path
+
+def plot_path(maze_matrix, solved_maze, path):
+    im = Image.new("RGB",(len(solved_maze[0]), len(solved_maze)))
+    pixels = []
+
+    for x in range(len(solved_maze)):
+        for y in range(len(solved_maze[0])):
+            pixel = (0, 0, 0)
+            if maze_matrix[x][y] == 0:
+                pixel = (255, 255, 255)
+
+            if solved_maze[x][y] > 0:
+                pixel = (0, 0, 255)
+
+            if (x, y) in path:
+                pixel = (255, 0, 0)
+
+            pixels.append(pixel)
+
+
+
+    im.putdata(pixels)
+    image = im.resize((400,400),Image.NEAREST)
+    return image
 
 
 if __name__ == '__main__':
